f7e41989f5773ae489a7b2d6b7d8329101ff2e1c
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-12-01 21:50:36 -0300

    ADD: Criaçao dos layouts

---
+++ src/layouts/auth/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { injectIntl } from "react-intl";
+const AuthThemes = ({ children }: any) => {
+  return <>{children}</>;
+};
+
+export default injectIntl(AuthThemes);
