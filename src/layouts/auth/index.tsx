97298cfafb361aa37092a50110f3951d2fd7674c
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-12-06 23:21:27 -0300

    PATCH: Internacionalizaçao no footer

--- src/layouts/auth/index.tsx
+++ src/layouts/auth/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { injectIntl } from "react-intl";
+import { Container } from "./styles";
 const AuthThemes = ({ children }: any) => {
-  return <>{children}</>;
+  return <Container>{children}</Container>;
 };
 
 export default injectIntl(AuthThemes);
