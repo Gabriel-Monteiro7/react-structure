100e3b0fce05f5247e0cde3307c4f571e3ac9bb3
Gabriel-Monteiro7 <gabrielmont713@gmail.com>
2020-12-06 23:26:01 -0300

    ADD: Inicializaçao das paginas de register e preview

---
+++ src/pages/Register/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { Typography as MuiTypography } from "@material-ui/core";
+export const Container = styled.div``;
+export const Typography = styled(MuiTypography)``;
+export const Title = styled(MuiTypography).attrs({ variant: "h4" })`
+  font-weight: 600;
+`;
