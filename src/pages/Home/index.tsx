94958ad9db99122985a0c99fccac52f88acf6ea8
Gabriel Monteiro <gabrielmont713@gmail.com>
2020-12-18 23:09:26 -0300

    ADD: Construçao dos cards na tela de dashboard

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,15 +2,43 @@ import React from "react";
 // import { useSelector, useDispatch } from "react-redux";
 // import { changeTheme, changeLanguage } from "~/store/modules/root/actions";
 // import { Typography, Switch } from "@material-ui/core";
-import { Container, Title } from "./styles";
+import {
+  Container,
+  Title,
+  ContainerCards,
+  ContainerCard,
+  Card,
+  Image,
+} from "./styles";
 import { useIntl } from "react-intl";
 function Home() {
   const intl = useIntl();
   return (
     <Container>
       <Title>{intl.formatMessage({ id: "homepage.title" })}</Title>
+      <ContainerCards>
+        <ContainerCard>
+          <Card>
+            <Image />
+          </Card>
+        </ContainerCard>
+        <ContainerCard>
+          <Card>
+            <Image />
+          </Card>
+        </ContainerCard>
+        <ContainerCard>
+          <Card>
+            <Image />
+          </Card>
+        </ContainerCard>
+        <ContainerCard>
+          <Card>
+            <Image />
+          </Card>
+        </ContainerCard>
+      </ContainerCards>
     </Container>
   );
 }
-
 export default Home;
--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -1,7 +1,43 @@
 import styled from "styled-components";
-import { Typography as MuiTypography } from "@material-ui/core";
+import {
+  Typography as MuiTypography,
+  Grid as MuiGrid,
+  Card as MuiCard,
+} from "@material-ui/core";
+
 export const Container = styled.div``;
 export const Typography = styled(MuiTypography)``;
+export const ContainerCards = styled(MuiGrid).attrs({
+  container: true,
+  justify: "space-between",
+  wrap: "wrap",
+  alignItems: "center",
+})`
+  margin: 20px 0px;
+`;
+
+export const ContainerCard = styled(MuiGrid).attrs({
+  container: true,
+  xs: 12,
+  lg: 3,
+  md: 6,
+  sm: 6,
+  item: true,
+})`
+  min-height: 25rem;
+`;
+export const Image = styled.div`
+  background-color: ${({ theme }) => theme.palette.grey["A400"] + 90};
+  height: 50%;
+  width: 100%;
+`;
+export const Card = styled(MuiCard).attrs({})`
+  background: ${({ theme }) => theme.palette.background.paper};
+  width: 100%;
+  margin: 10px;
+  border-radius: 6px;
+`;
+
 export const Title = styled(MuiTypography).attrs({ variant: "h4" })`
   font-weight: 600;
 `;
