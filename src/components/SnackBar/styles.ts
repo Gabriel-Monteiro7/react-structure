24bd4097cb43114ac02f46338aeea6803d60e385
Gabriel Monteiro <gabrielmont713@gmail.com>
2020-12-29 21:51:00 -0300

    ADD: Inicializaçao do componente de snackBar

---
+++ src/components/SnackBar/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import {
+  Snackbar,
+  Typography as MuiTypography,
+  CircularProgress as MuiCircularProgress,
+} from "@material-ui/core";
+import { Alert } from "@material-ui/lab";
+export const Container = styled(Snackbar).attrs({
+  anchorOrigin: {
+    vertical: "top",
+    horizontal: "right",
+  },
+})``;
+export const Content = styled(Alert).attrs({ variant: "filled" })`
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 15px;
+  display: flex;
+  background-color: ${({ theme, color }: any) =>
+    color === "info" && theme.palette.grey[400]};
+
+  .MuiAlert-icon {
+    border-right: 1px solid #ffffff50;
+    padding: 0;
+    padding-right: 9px;
+    margin-right: 9px;
+  }
+`;
+export const Typography = styled(MuiTypography)``;
+export const CircularProgress = styled(MuiCircularProgress).attrs({ size: 20 })`
+  color: #ffffff;
+`;
